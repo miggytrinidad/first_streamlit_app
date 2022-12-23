2ce02852b43f552456416e86a9dc3ada388ede2f
miggytrinidad <miggytrinidad00@gmail.com>
2022-12-22 17:22:39 -0800

    Update streamlit_app.py
    added snowflake integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,14 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # display table on page
 streamlit.dataframe(fruityvice_normalized)
 
+#Snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 
 
